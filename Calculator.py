76fa0e382b100b77338875ef46dabab7de8dbfee
Wblake95 <73808142+Wblake95@users.noreply.github.com>
2020-11-03 09:34:37 -0800

    add div2 and modulo
    add div 2 and modulo
    add more comment

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,18 @@
 """
-Apparently, this is how you dod a multiline comment in python.
+Apparently, this is how you do a multiline comment in python.
 I wanted to give it a shot and thought I could try out the whole branching thing as well.
 learned from geeksforgeeks.org
 """
+'''
+new stuff from geeksforgeeks.org
+this is a 'Division(float) of number' whatever that means
+and a 'modulo of both numbers'
+I will be testing this today
+'''
 print("Choose first int:")
 x = int(input())
-print("Choose operator (+,-,*,/):")
+print("Choose operator (+,-,*,/,//,%):")
+print("/ = with decimal, // = without decimal. % = ramainder")
 mode = input()
 print("Choose second int:")
 y = int(input())
@@ -19,3 +26,11 @@ elif mode == "*":
     print(x*y)
 elif mode == "/":
     print(x/y)
+# 'Division(float) of number'
+# this just gets ride of teh decimal point
+elif mode == "//":
+    print(x//y)
+# modulo of both numbers
+# the gets teh remainder
+elif mode == "%":
+    print(x%y)
